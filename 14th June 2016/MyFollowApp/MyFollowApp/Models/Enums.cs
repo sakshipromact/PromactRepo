39bced23548a80d35fc3b5620154b9c3c3d55306
sakshi promact <sakshi@promactinfo.com>
2016-06-14 18:59:07 +0530

    AngularJS with Owin auth and web api

---
+++ 14th June 2016/MyFollowApp/MyFollowApp/Models/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFollowApp.Models
+{
+    public class Enums
+    {
+        public enum ApplicationTypes
+        {
+            JavaScript = 0,
+            NativeConfidential = 1
+        };
+    }
+}
